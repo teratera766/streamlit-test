88af194c24f4db223d1b71f43b06280e8342bd7e
teratera766 <teratera766@gmail.com>
2024-10-21 08:40:20 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import random
+from PIL import Image
+
+st.title("おみくじ")
+
+image = Image.open('4.png')
+
+st.image(image,width=100)
+
+def uranau():
+    ph = ('1.png', '2.png', '3.png')
+    rn_ph = random.choice(ph)
+
+    image = Image.open(rn_ph)
+
+    st.image(image,width=100)
+
+
+if st.button('占う'):
+    uranau()
+
+
+
